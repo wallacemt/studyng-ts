eeeafaf331de7e04fe3d4c31adae37ac4dcadc85
Wallace Santana <wallacesantanak0@gmail.com>
2025-07-21 20:19:28 -0300

    feat(lesson17): lesson for migration js for ts with segurance

---
+++ lesson17/index.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param {number} arg1
+ * @param {number} arg2
+ * @returns number
+ */
+function sum(arg1, arg2) {
+  return arg1 + arg2;
+}
---
+++ lesson17/index.ts
@@ -0,0 +1,3 @@
+function sum(arg1: number, arg2: number): number {
+  return arg1 + arg2;
+}
